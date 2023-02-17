15eb3dd25e38bb13068ba83a10ad7acb9bbce4b7
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-02-17 17:50:04 -0500

    created schema, seeds.sql.  Wrote pseudocode. Started server.js

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+​
+USE employeetracker_db;
+​
+CREATE TABLE department (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    name VARCHAR(30)
+);
+​
+​CREATE TABLE role (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    department_id: INT 
+    FOREIGN KEY (department_id) REFERENCES department(id)  
+);
+​​CREATE TABLE employee (
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id: INT,
+    manager_id: INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) 
+);
---
+++ server.js
@@ -0,0 +1,141 @@
+//libraries 
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Purr2023thos!',
+        database: 'employeetracker_db'
+    },
+    console.log(`Connected to the employeetracker_db database.`)
+);
+
+
+const questions =
+    [
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'options',
+            choices: ['View all Departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit'],
+        },
+    ]
+
+
+function trackerChoice(response) {
+    switch (response.options) {
+        case 'View all Departments':
+            allDepartments();
+            break;
+        case 'View all roles':
+            allRoles();
+            break;
+        case 'View all employees':
+            allEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        case 'Update an employee role':
+            updateEmployee();
+            break;
+        case 'Quit':  //might need something to actually end the program.  This just ends the switch statement
+            console.log("Thank you, the program will now end.");
+            process.exit(code);
+   
+    }
+};
+
+function allDepartments() {
+
+};
+
+function allRoles() {
+
+};
+
+function allEmployees() {
+
+};
+
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the department name?',
+                name: 'deptname',
+            },
+
+        ])
+};
+
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the role?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'What is the salary of the role?',
+                name: 'salary',
+            },
+            {
+                type: 'input',
+                message: 'To which department does the role belong?',
+                name: 'department_id',
+            },
+        ])
+};
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the first name of the employee?',
+                name: 'first_name',
+            },
+            {
+                type: 'input',
+                message: 'What is the last name of the employee?',
+                name: 'last_name',
+            },
+            {
+                type: 'input',
+                message: 'What is their role?',
+                name: 'role_id',
+            },
+            {
+                type: 'input',
+                message: 'What is the manager for the role?',
+                name: 'manager_id',
+            },
+        ])
+};
+
+function updateEmployee() {
+
+};
